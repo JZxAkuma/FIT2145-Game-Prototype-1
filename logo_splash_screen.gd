614b107023bcdd3f6c5d3b92ca4589f8845c87db
JZxAkuma <76819854+JZxAkuma@users.noreply.github.com>
2026-08-07 22:14:45 +1000

    Splash screen tweeks
    + Logo last 0.5 seconds longer
    + left click to skip splash screen

--- logo_splash_screen.gd
+++ logo_splash_screen.gd
@@ -6,9 +6,14 @@ extends CanvasLayer
 func _ready() -> void:
 	anim_player.play("fade_in")
 
+#func _unhandled_input(event: InputEvent) -> void:
+	#if event.is_action_pressed("shoot"):
+		#get_tree().change_scene_to_file("res://world.tscn")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("shoot"):
+		get_tree().change_scene_to_file("res://world.tscn")
 
 func _fade_in_done():
 	anim_player.play("fade out")
