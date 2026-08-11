cb0b2ddae68d01b83cd67eef624d79670a0a120e
JZxAkuma <76819854+JZxAkuma@users.noreply.github.com>
2026-08-11 12:22:22 +1000

    Transition Tweaks
    + Transition now truly wait for next level to load before fading out

--- transition.gd
+++ transition.gd
@@ -12,11 +12,23 @@ func _process(delta: float) -> void:
 	pass
 
 
+#func _transition_scene(scene: String):
+	#animation_player.play("fade_in")
+	#await animation_player.animation_finished
+#
+	#get_tree().change_scene_to_file(scene)
+#
+	#await get_tree().process_frame
+	#animation_player.play("fade_out")
+
 func _transition_scene(scene: String):
 	animation_player.play("fade_in")
 	await animation_player.animation_finished
 
+	var old_scene = get_tree().current_scene
 	get_tree().change_scene_to_file(scene)
 
-	await get_tree().process_frame
+	while get_tree().current_scene == old_scene:
+		await get_tree().process_frame
+
 	animation_player.play("fade_out")
