ea2a5faa9ce49fb3159ea959300a05fa90a29ab1
JZxAkuma <76819854+JZxAkuma@users.noreply.github.com>
2026-08-16 23:59:22 +1000

    Fire particle + light

--- fire_blast_particle.gd
+++ fire_blast_particle.gd
@@ -6,14 +6,13 @@ func _ready() -> void:
 	self.emitting = true
 	_selfdestruct()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$OmniLight3D.light_energy -= 5 * delta
+	if $OmniLight3D.light_energy <= 0:
+		$OmniLight3D.light_energy = 0
 
 func _selfdestruct():
 	timer.start()
-
 
 func _on_timer_timeout() -> void:
 	self.queue_free()
--- fireball.gd
+++ fireball.gd
@@ -3,6 +3,7 @@ var target = null
 var target_position: Vector3
 @export var speed: float = 20.0
 @export var hit_radius: float = 0.5
+@onready var blast_particle = preload("res://fire_blast_particle.tscn")
 
 func _physics_process(delta: float) -> void:
 	var to_target = target_position - global_position
@@ -13,17 +14,21 @@ func _physics_process(delta: float) -> void:
 		if is_instance_valid(target) and target.has_method("_set_fire"):
 			target._set_fire()
 		global_position = target_position
-		queue_free()
+		_splash()
 		return
 
 	look_at(target_position, Vector3.UP)
 	global_position += -global_transform.basis.z * step
 
-func _on_area_entered(area: Area3D) -> void:
-	self.queue_free()
 
 func _on_body_entered(body: Node3D) -> void:
 	if body == target:
 		if body.has_method("_set_fire"):
 			body._set_fire()
-		queue_free()
+		_splash()
+		
+func _splash():
+	var part = blast_particle.instantiate()
+	get_tree().current_scene.add_child(part)
+	part.global_position = self.global_position
+	self.queue_free()
