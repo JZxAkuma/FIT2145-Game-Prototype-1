ad3459415ff312fd4c0d6401c6fe0480412abca6
JZxAkuma <76819854+JZxAkuma@users.noreply.github.com>
2026-08-16 22:09:21 +1000

    Flat shroom
    Flat shroom implemented

---
+++ level component/flat_shroom.gd
@@ -0,0 +1,29 @@
+extends Node3D
+@export var bounce_scale = 0.8
+var bounce_velocity = 11
+var cap_tween : Tween
+@onready var cap = $Area3D/Cap
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		if cap_tween:
+			cap_tween.kill()
+		
+		cap_tween = create_tween()
+		cap.scale = Vector3(bounce_scale,bounce_scale,bounce_scale)
+		cap_tween.tween_property(cap, "scale", Vector3.ONE, 0.2)
+		_bounce(body)
+
+func _bounce(player: Node3D):
+	player.velocity.y = 0
+	player.velocity.y = bounce_velocity
+	
