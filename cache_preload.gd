12512975cd1f9cdc4fb6d0d13065443cfedf7c8d
JZxAkuma <76819854+JZxAkuma@users.noreply.github.com>
2026-08-20 16:17:15 +1000

    Added pre caching
    + Pre load everything. In theory it would reduce stuttering when loading new object

---
+++ cache_preload.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+@export var scenes_to_preload: Array[PackedScene] = []
+@onready var progress_label = $CanvasLayer/Control/Label  # optional, for a loading %
+
+var loaded_count := 0
+
+func _ready() -> void:
+	await get_tree().process_frame  # let the loading screen actually render first
+	_warm_up_shaders()
+
+
+func _warm_up_shaders() -> void:
+	for scene in scenes_to_preload:
+		var instance = scene.instantiate()
+		add_child(instance)
+
+		# force at least one render frame with this thing on screen
+		# (off in a far corner, or behind the camera) so its shaders compile
+		if instance is Node3D:
+			instance.position = Vector3(0, -9999, 0)
+
+		await get_tree().process_frame
+		await get_tree().process_frame
+
+		loaded_count += 1
+		if progress_label:
+			progress_label.text = "Pre cashing\n %d / %d" % [loaded_count, scenes_to_preload.size()] + " item(S)"
+
+		instance.queue_free()
+		await get_tree().process_frame
+
+	get_tree().change_scene_to_file("res://logo_splash_screen.tscn")
